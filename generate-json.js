2421540275aca9d76c1ce9d6246ed9e14bbc1e00
Steepee <114439934+Steppi1@users.noreply.github.com>
2025-04-13 13:46:58 +0200

    sasd

--- generate-json.js
+++ generate-json.js
@@ -9,3 +9,6 @@ const files = fs
 const output = files.map(f => `https://raw.githubusercontent.com/Steppi1/tests/main/imgs/${f}`);
 
 fs.writeFileSync("images.json", JSON.stringify(output, null, 2));
+
+
+console.log("images.json generato con", output.length, "immagini.");
