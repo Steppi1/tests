8329f5d6ee07b2ef8c62d5542ecbf4ba1ccc8bfd
Steepee <114439934+Steppi1@users.noreply.github.com>
2025-05-21 21:59:16 +0200

    go

---
+++ quartz.config.ts
@@ -0,0 +1,12 @@
+
+export default {
+  siteMetadata: {
+    title: "Stefano – Portfolio",
+    author: "Stefano Girard",
+    description: "Sketch, progetti e note",
+    siteUrl: "https://tuosito.com"
+  },
+  defaultLang: "it",
+  contentDir: "content",
+  staticDir: "static"
+}
